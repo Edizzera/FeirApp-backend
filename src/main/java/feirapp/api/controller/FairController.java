9af6545925836029d11f164b8140e6a5c93dbac7
Edizzera <moraisedi.dev@gmail.com>
2023-09-25 20:44:22 -0300

    ajusta repository

--- src/main/java/feirapp/api/controller/FairController.java
+++ src/main/java/feirapp/api/controller/FairController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import feirapp.api.dto.InFair;
 import feirapp.model.Fair;
+import feirapp.repository.FairRepository;
 import feirapp.service.FairService;
 import jakarta.enterprise.inject.Produces;
 import jakarta.inject.Inject;
@@ -14,33 +15,38 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.MediaType;
+import lombok.AllArgsConstructor;
 
 @Path("/api/v1/fairs")
+@AllArgsConstructor
 public class FairController {
     
-    // @Inject
-    // private final FairService fairService;
+    @Inject
+    private final FairRepository fairRepository;
     @GET
     // @Produces(MediaType.APPLICATION_JSON)
     public List<Fair> findAll() {
-        return List.of(new Fair());
+        return fairRepository.listAll(null);
     }
     
     @GET
     @Path("/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Fair findFairById(@PathParam("id") String id) {
-            return new Fair();
+    public Fair findFairById(@PathParam("id") Long id) {
+            return fairRepository.findById(id);
 
     }
 
     @POST
     public Fair create(InFair inFair) {
-        return new Fair();
+        Fair newFair = new Fair(1,"Nova Feira",new Fair.Address("Duque de Caxias","Centro Histórico",1234));
+        fairRepository.persist(newFair);
+        return newFair;
     }
 
     @DELETE
     public void delete(Long id) {
+        fairRepository.deleteById(id);
 
     }
 
--- src/main/java/feirapp/api/dto/InFair.java
+++ src/main/java/feirapp/api/dto/InFair.java
@@ -1,11 +1,13 @@
 package feirapp.api.dto;
 
 import feirapp.model.Fair.Address;
+import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class InFair {
 
     private String name;
